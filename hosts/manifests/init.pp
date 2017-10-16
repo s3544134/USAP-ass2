a5416419caa0ecd53f488c0b7db8f2d92291ab9c
s3544134 <32807062+s3544134@users.noreply.github.com>
2017-10-16 15:46:25 +1100

    Update init.pp

--- hosts/manifests/init.pp
+++ hosts/manifests/init.pp
@@ -1,1 +1,17 @@
-
+#Host file entries for RMIT Csit servers
+class hosts{
+  host{ 'jupiter.csit.rmit.edu.au':
+    ip => '131.170.5.135',
+    host_aliases => 'jupiter',
+  }
+  
+  host { 'saturn.csit.rmit.edu.au':
+    ip => '131.170.5.132',
+    host_aliases => 'saturn',
+  }
+  
+  host { 'titan.csit.rmit.edu.au':
+    ip => '131.170.5.131',
+    host_aliases => 'titan',
+  }
+}
