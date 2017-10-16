eba31df70f5dc2e6280d82f3835290900c1cc2eb
s3544134 <32807062+s3544134@users.noreply.github.com>
2017-10-16 16:09:05 +1100

    Update init.pp

--- openssh/manifests/init.pp
+++ openssh/manifests/init.pp
@@ -1,1 +1,20 @@
+# Run ssh and set config file
 
+class openssh
+{
+  file { '/etc/ssh/sshd_config':
+    ensure  => present,
+    mode    => '0777',
+    owner   => 'root',
+    group   => 'root',
+    source  => '/etc/puppetlabs/code/environments/production/manifests/configfiles/sshd_config',
+    require => Package['openssh-server'],
+  }
+  
+  -> service { 'sshd' :
+    ensure    => running,
+    enable    => true,
+    require   => Package['openssh-server'],
+    subscribe => File['/etc/ssh/sshd_config'],
+  }
+}
