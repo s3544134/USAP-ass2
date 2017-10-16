32e3b447a9e3dc49283b301b674cfbe39232c6a7
s3544134 <32807062+s3544134@users.noreply.github.com>
2017-10-16 15:44:49 +1100

    Update init.pp

--- groups/manifests/init.pp
+++ groups/manifests/init.pp
@@ -1,1 +1,14 @@
-
+#create Groups
+class groups{
+	group { 'sysadmin':
+		ensure => 'present',
+		}
+	
+	group { 'trucks':
+		ensure => 'present',
+		}
+	
+	group { 'cars':
+		ensure => 'present',
+		}
+}
