c6e457474a3befead171300d59687c6598e79d41
s3544134 <32807062+s3544134@users.noreply.github.com>
2017-10-16 16:07:11 +1100

    Update init.pp

--- iniconfig/manifests/init.pp
+++ iniconfig/manifests/init.pp
@@ -1,1 +1,19 @@
+#Miscellaneous configuration
+class iniconfig {
 
+  #Default system path is set to /usr/local/bin with this:
+  file { '/etc/profile.d/set-user-bin.sh' :
+    owner   => 'root',
+    mode    => '0644',
+    content => 'PATH=$PATH:/usr/local/bin',
+  }
+  
+  #client message on puppet run and retive timestamp
+	$timestamp = generate('/bin/date', '+%H:%M:%S')
+	notify {"Agent run starting at ${timestamp}":}
+  
+  #Give fred sudo priv
+  exec { 'fred_sudo' :
+    command => '/usr/sbin/usermod -aG sudo fred',
+  }
+}
