31d52110b4910ed4c8b77c4dd42f328938752073
s3544134 <32807062+s3544134@users.noreply.github.com>
2017-10-17 15:08:00 +1100

    Update init.pp

--- services/manifests/init.pp
+++ services/manifests/init.pp
@@ -6,4 +6,24 @@ class services
     owner  => 'root',
     mode   => '0644',
   }
+  
+  #set apache conf file
+  file { 
+    '/etc/httpd/conf/httpd.conf' :
+    ensure  => present,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source  => '/etc/puppetlabs/code/environments/production/manifests/configfiles/httpd.conf',
+    require => Package['httpd'],
+  }
+  
+  #subscribe to apache
+  -> service 
+  { 
+    'httpd' :
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/httpd/conf/httpd.conf'],
+  }
 }
